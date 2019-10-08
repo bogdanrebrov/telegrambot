87bef4ab23f32d958d1650d869275bb3f0cf0091
paperplane90 <b.rebrov@gmail.com>
2019-10-08 16:30:52 +0300

    Delete bot.js

--- bot.js
+++
@@ -1,6 +0,0 @@
-//init bot
-const TeleBot = require('telebot');
-const bot = new TeleBot('461721692:AAHKxW50YSFcZpohLNVloouPWJGHEkKHSfk');
-//first command
-bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));   
-bot.start();
